12feb66f651549347a7f903716bd8258876d68af
Joel Silva <53149347+zoreu@users.noreply.github.com>
2022-12-29 14:43:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 exports.run = function(context) {
-  return {
-    "results": [
+  // Verifica o tipo de conteúdo que o usuário está tentando acessar
+  if (context.type === "movie") {
+    // Se o usuário estiver tentando acessar um catálogo de filmes, retorna uma lista de filmes
+    return {
+      "results": [
+        {
+          "name": "Filme 1",
+          "description": "Um filme incrível!",
+          "poster": "https://i.imgur.com/Xt1WLzp.png"
+        },
+        {
+          "name": "Filme 2",
+          "description": "Outro filme incrível!",
+          "poster": "https://i.imgur.com/Xt1WLzp.png"
+        }
+      ]
+    };
+  } else if (context.type === "series") {
+    // Se o usuário estiver tentando acessar um catálogo de séries, retorna uma lista de séries
+    return {
+      "results": [
+        {
+          "name": "Série 1",
+          "description": "Uma série incrivel"
+        "poster": "https://i.imgur.com/Xt1WLzp.png"
+        },
       {
-        "name": "Mensagem",
-        "description": "Olá, mundo!",
-        "poster": "./images/wallpapaer.jpg"
+        "name": "Série 2",
+        "description": "Outra série incrível!",
+        "poster": "https://i.imgur.com/Xt1WLzp.png"
       }
     ]
   };
+}
 };
+          
