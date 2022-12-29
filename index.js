4debfac05b99d38dba620b04170dc7c272bf3400
Joel Silva <53149347+zoreu@users.noreply.github.com>
2022-12-29 14:33:06 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+exports.run = function(context) {
+  return {
+    "results": [
+      {
+        "name": "Mensagem",
+        "description": "Olá, mundo!",
+        "poster": "./images/wallpapaer.jpg"
+      }
+    ]
+  };
+};
